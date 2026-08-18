1bbeecdf9ac7effda6b8b260b2217d8c276f7a14
Simon Batardiere <simon.batardiere@protonmail.com>
2026-08-18 23:51:58 +0200

    feat(error): add internal error code
    - map it on HTTP status code 500

--- src/error.rs
+++ src/error.rs
@@ -8,6 +8,9 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum Error {
+    #[error("internal error")]
+    InternalError,
+
     #[error("user '{0}' already exists")]
     UserAlreadyExists(String),
 
@@ -25,6 +28,7 @@ pub struct ErrorResponse {
 impl IntoResponse for Error {
     fn into_response(self) -> Response {
         let (status, code) = match &self {
+            Error::InternalError => (StatusCode::INTERNAL_SERVER_ERROR, "INTERNAL_ERROR"),
             Error::UserAlreadyExists(_) => (StatusCode::CONFLICT, "USER_ALREADY_EXISTS"),
             Error::UserNotFound(_) => (StatusCode::NOT_FOUND, "USER_NOT_FOUND"),
         };
@@ -38,6 +42,13 @@ impl IntoResponse for Error {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn should_map_internal_error_to_500() {
+        let response = Error::InternalError.into_response();
+
+        assert_eq!(response.status(), StatusCode::INTERNAL_SERVER_ERROR);
+    }
 
     #[test]
     fn should_map_user_already_exists_to_409() {
